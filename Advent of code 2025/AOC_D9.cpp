ae0ba5687625ae72c38addc5d461f15946b675a4
Iustin <ciobicaiustin2@gmail.com>
2025-12-09 16:22:03 +0200

    Day 9 save

--- Advent of code 2025/AOC_D9.cpp
+++ Advent of code 2025/AOC_D9.cpp
@@ -13,10 +13,45 @@ typedef struct redCoords
 	int y;
 };
 
-int isWithingBounds(redCoords* redPosition, redCoords* otherCorners)   // compare with the other corners with the red boxes and check if theyre inside(smaller than them)
+typedef struct edges
 {
+	int edge[1000] = { 0 };
+	int size;
+};
 
+void getEdges(edges* edgeH, edges* edgeV, redCoords* redPositions, int indexPos)
+{
+	int indexH, indexV;
+	indexH = indexV = 0;
+
+	for (int i = 1; i < indexPos; i++)
+	{
+		redCoords corner1 = redPositions[i];
+		redCoords corner2 = redPositions[i-1];
+
+		if (corner1.x == corner2.x)
+		{
+			edgeH->edge[indexV] = corner1.x;
+			indexV++;
+		}
+
+		if (corner1.y == corner2.y)
+		{
+			edgeV->edge[indexH] = corner1.y;
+			indexH++;
+		}
+	}
+
+	edgeH->size = indexH;
+	edgeV->size = indexV;
 }
+
+//int isWithingBounds(redCoords A, redCoords B, redCoords C, redCoords D, redCoords* redPositions)   // compare with the other corners with the red boxes and check if theyre inside(smaller than them)
+//{
+//
+//
+//	return 0;
+//}
 
 void part1()
 {
@@ -40,8 +75,11 @@ void part1()
 	for (int i = 0; i < indexPos - 1; i++)
 		for (int j = i + 1; j < indexPos; j++)
 		{
-			long long int xDif = abs(redPositions[i].x - redPositions[j].x) + 1;
-			long long int yDif = abs(redPositions[i].y - redPositions[j].y) +  1;
+			redCoords A = redPositions[i];
+			redCoords C = redPositions[j];
+
+			long long int xDif = abs(A.x - C.x) + 1;
+			long long int yDif = abs(A.y - C.y) + 1;
 
 			long long int surfArea = xDif * yDif;
 
@@ -71,64 +109,39 @@ void part2()
 
 	long long int maxSurfArea = -1;
 
-	for (int i = 0; i < indexPos - 1; i++)
-		for (int j = i + 1; j < indexPos; j++)
-		{
-			long long int xDif = abs(redPositions[i].x - redPositions[j].x) + 1;
-			long long int yDif = abs(redPositions[i].y - redPositions[j].y) + 1;
+	edges edgeH;    // find the parameter of polygon
+	edges edgeV;
 
-			long long int surfArea = xDif * yDif;
+	getEdges(&edgeH, &edgeV, redPositions, indexPos);
 
-			if (surfArea > maxSurfArea)
-				maxSurfArea = surfArea;
-		}
+	cout << "horizontal edges" << endl;
+	for (int i = 0; i < edgeH.size; i++)
+		cout << edgeH.edge[i] << endl;
 
-	cout << maxSurfArea;
-}
+	//for (int i = 0; i < indexPos - 1; i++)
+	//	for (int j = i + 1; j < indexPos; j++)
+	//	{
+	//		redCoords A = redPositions[i];
+	//		redCoords C = redPositions[j];
+	//		redCoords B = { C.x, A.y };
+	//		redCoords D = { A.x, C.y };
 
-void part2idea()
-{
-	ifstream input("input.txt");
+	//		//if()
 
-	char line[100];
-	redCoords redPositions[500];
-	int indexPos = 0;     // no of elements from input
+	//		//  has to be done after confirming the rectangle is in polygon
+	//		long long int xDif = abs(A.x - C.x) + 1;
+	//		long long int yDif = abs(A.y - C.y) + 1;
 
-	while (input >> line)
-	{
-		sscanf(line, "%d,%d", &redPositions[indexPos].y, &redPositions[indexPos].x);
-		indexPos++;
-	}
+	//		long long int surfArea = xDif * yDif;
 
-	for (int i = 0; i < indexPos; i++)
-		cout << redPositions[i].y << " " << redPositions[i].x << endl;
+	//		if (surfArea > maxSurfArea)
+	//			maxSurfArea = surfArea;
+	//	}
 
-	long long int maxSurfArea = -1;
-
-	for (int i = 0; i < indexPos - 1; i++)
-		for (int j = i + 1; j < indexPos; j++)
-		{
-
-			redCoords otherCorners[2];
-			otherCorners[0].x = redPositions[i].x; otherCorners[0].y = redPositions[j].y;
-			otherCorners[1].x = redPositions[j].x; otherCorners[1].y = redPositions[i].x;
-
-			if ()   // if the other 2 corners are within the bounds
-			{
-				long long int xDif = abs(redPositions[i].x - redPositions[j].x) + 1;
-				long long int yDif = abs(redPositions[i].y - redPositions[j].y) + 1;
-
-				long long int surfArea = xDif * yDif;
-
-				if (surfArea > maxSurfArea)
-					maxSurfArea = surfArea;
-			}
-		}
-
-	cout << maxSurfArea;
+	//cout << maxSurfArea;
 }
 
 int main()
 {
-	part1();
+	part2();
 }
