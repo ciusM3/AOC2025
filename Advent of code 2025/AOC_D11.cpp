302dbc2a506d396d5e451e0c2ecec115aed50df4
Iustin <ciobicaiustin2@gmail.com>
2025-12-12 00:53:43 +0200

    Day 11 part 2 start

--- Advent of code 2025/AOC_D11.cpp
+++ Advent of code 2025/AOC_D11.cpp
@@ -14,25 +14,24 @@ typedef struct node
 	int isVisited;
 };
 
-void dfs(node* currentNode, node* out, int& count)
+int dfs(node* currentNode, node* out)
 {
 	if (currentNode == out)
-	{
-		count++;
-		return;
-	}
+		return 1;
 
 	currentNode->isVisited = 1;
+
+	int noPaths = 0;
 
 	for (node* i : currentNode->neighbours)
 	{
 		if (i->isVisited == 0)
-		{
-			dfs(i, out, count);
-		}
+			noPaths += dfs(i, out);
 	}
 
 	currentNode->isVisited = 0;
+
+	return noPaths;
 }
 
 void part1()
@@ -47,7 +46,7 @@ void part1()
 	
 	while (input.getline(line, 1000))
 	{
-		strcpy(inputCopy[inputIndex], line);
+		strcpy(inputCopy[inputIndex], line);     // make a copy of the input to help create connections
 		inputIndex++;
 
 		// first element is always original node
@@ -100,9 +99,77 @@ void part1()
 		i->isVisited = 0;
 	}
 
-	int count = 0;
+	int count = dfs(you, out);
 
-	dfs(you, out, count);
+	cout << "answer: " << count;
+}
+
+void part2()
+{
+	ifstream input("input.txt");
+
+	char line[1000];
+	char inputCopy[600][1000];
+	int inputIndex = 0;
+
+	vector<node*> nodes;
+
+	while (input.getline(line, 1000))
+	{
+		strcpy(inputCopy[inputIndex], line);     // make a copy of the input to help create connections
+		inputIndex++;
+
+		// first element is always original node
+		node* aux = new node;
+		char* p = strtok(line, ":");
+		aux->name = string(p);
+		aux->neighbours = {};
+		aux->isVisited = 0;
+		nodes.push_back(aux);
+	}
+
+	// add out as node, because it isn't present as one in input
+	node* out = new node;
+	out->name = "out";
+	out->isVisited = 0;
+	out->neighbours = {};
+	nodes.push_back(out);
+
+	// connect the graph
+	for (int i = 0; i < inputIndex; i++)
+	{
+		char* p = strtok(inputCopy[i], ":");
+		p = strtok(NULL, " ");
+		while (p)
+		{
+			for (node* j : nodes)
+			{
+				if (j->name == string(p))
+				{
+					nodes[i]->neighbours.push_back(j);
+					break;
+				}
+			}
+			p = strtok(NULL, " ");
+		}
+	}
+
+	node* svr = nullptr;
+	for (node* i : nodes)
+	{
+		if (i->name == "svr")
+		{
+			svr = i;
+			break;
+		}
+	}
+
+	for (node* i : nodes)
+	{
+		i->isVisited = 0;
+	}
+
+	int count = dfs(svr, out);
 
 	cout << "answer: " << count;
 }
